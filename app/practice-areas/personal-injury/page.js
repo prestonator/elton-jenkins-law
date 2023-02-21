43da27d2a19115676790fa7f3ebfafd7ec1c6716
prestonator <preston.b.jenkins-1@ou.edu>
2023-02-21 15:51:59 -0600

    e

--- app/practice-areas/personal-injury/page.js
+++ app/practice-areas/personal-injury/page.js
@@ -1,4 +1,5 @@
 import { getMediaData } from "@/src/api/fetchData/fetchMedia";
+import styles from "@/src/styles/pages/PracticeArea.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
