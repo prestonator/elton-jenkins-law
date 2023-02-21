4bf6b04ceafc03d7b6b6f32052c582dbeaab22a5
prestonator <preston.b.jenkins-1@ou.edu>
2023-02-21 17:34:37 -0600

    added a lil more

--- app/practice-areas/personal-injury/page.js
+++ app/practice-areas/personal-injury/page.js
@@ -1,13 +1,59 @@
-import { getMediaData } from "@/src/api/fetchData/fetchMedia";
+import { getPracticePageData } from "@/src/api/fetchData/fetchPracticePage";
 import styles from "@/src/styles/pages/PracticeArea.module.css";
-import Image from "next/image";
-import Link from "next/link";
+import ReactMarkdown from "react-markdown";
+import remarkBreaks from "remark-breaks";
+import FlipCard from "@/src/components/ServiceCard";
+
 
 
 export default async function Page() {
+	const pageData = await getPracticePageData("personal");
 	return (
 		<>
-			<section className={styles.sectionOne} id="section"></section>
+			<section
+				className={styles.sectionOne}
+				style={{
+					backgroundImage: `url(${pageData.heroImage})`,
+					backgroundSize: "cover",
+				}}
+			>
+				<div className={styles.row}>
+					<div className={styles.textBlurb}>
+						<ReactMarkdown
+							remarkPlugins={[remarkBreaks]}
+						>{`${pageData.heroText}`}</ReactMarkdown>
+					</div>
+				</div>
+			</section>
+			<section className={styles.sectionTwo}>
+				<div className={`${styles.col} ${styles.colOne}`}>
+					<ReactMarkdown>{`${pageData.firstHeading}`}</ReactMarkdown>
+				</div>
+				<div className={`${styles.col} ${styles.colTwo}`}>
+					<ReactMarkdown
+						remarkPlugins={[remarkBreaks]}
+					>{`${pageData.firstContent}`}</ReactMarkdown>
+				</div>
+			</section>
+			<section className={styles.sectionThree}>
+				<div className={`${styles.row} ${styles.rowOne}`}>
+					<ReactMarkdown
+						remarkPlugins={[remarkBreaks]}
+					>{`${pageData.secondHeading}`}</ReactMarkdown>
+				</div>
+				<div className={`${styles.row} ${styles.rowTwo}`}>
+					{pageData.card.map((card) => (
+						<FlipCard
+							key={card.id}
+							title={card.title}
+							excerpt={card.excerpt}
+							content={card.content}
+							image={card.image}
+							href=""
+						/>
+					))}
+				</div>
+			</section>
 		</>
 	);
 }
